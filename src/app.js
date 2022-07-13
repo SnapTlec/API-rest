6e27add6280f50d1df23782b497f7cd7a96814af
Léo Brito <Leonardobrito371@gmail.com>
2022-07-13 23:58:28 +0000

    Remoção das rotas do arquivo app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import db from './config/dbConnect.js'
 import livros from './models/Livro.js'
+import routes from './routes/index.js'
 
 
 db.on('erro', console.log.bind(console, 'Error de conexão'))
@@ -12,29 +13,15 @@ const app = express()
 
 app.use(express.json())
 
-// const livros = [
-//     {id: 1, 'titulo': 'Senhor dos Aneis'},
-//     {id: 2, 'titulo': 'O Hobiit'}
-// ]
+routes(app)
+// app.put('/livros/:id', (req, res) => {
+//     let index = buscaLivro(req.params.id)
+//     livros[index].titulo = req.body.titulo
+//     res.json(livros)
+// })
 
-app.get('/', (req, res) => {
-    res.status(200).send('Curso de Node')
-})
-
-
-app.post('/livros', (req, res) => {
-    livros.push(req.body);
-    res.status(201).send('Livros cadastrado com sucesso')
-})
-
-app.put('/livros/:id', (req, res) => {
-    let index = buscaLivro(req.params.id)
-    livros[index].titulo = req.body.titulo
-    res.json(livros)
-})
-
-function buscaLivro(id){
-    return livros.findIndex(livro => livro.id == id)
-}
+// function buscaLivro(id){
+//     return livros.findIndex(livro => livro.id == id)
+// }
 
 export default app
