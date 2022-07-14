dce5498e4a1fbf856195357127f6a5d6f271d9ca
Léo Brito <Leonardobrito371@gmail.com>
2022-07-14 14:09:45 +0000

    Remoção de codigo comentado

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import db from './config/dbConnect.js'
 import routes from './routes/index.js'
 
 
-
 db.on('erro', console.log.bind(console, 'Error de conexão'))
 db.once("open", () =>{
     console.log('conexão com o banco feita com sucesso!!')
@@ -14,14 +13,5 @@ const app = express()
 app.use(express.json())
 
 routes(app)
-// app.put('/livros/:id', (req, res) => {
-//     let index = buscaLivro(req.params.id)
-//     livros[index].titulo = req.body.titulo
-//     res.json(livros)
-// })
-
-// function buscaLivro(id){
-//     return livros.findIndex(livro => livro.id == id)
-// }
 
 export default app
