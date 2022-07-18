bb05777ee40d92f54f97b4f199f79bdbc982457b
Léo Brito <Leonardobrito371@gmail.com>
2022-07-18 22:44:42 +0000

    Atualização do schema de usuários

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -3,9 +3,25 @@ import mongoose from "mongoose"
 const {Schema} = mongoose
 
 const usuarioSchema = new Schema({
-    id: {type : String},
-    email: {type : String, required : true},
-    senha: {type: String, required: true}
+    id: {
+        type : String
+    },
+    email: {
+        type : String,
+        unique: true, 
+        required : true,
+        lowercase: true
+    },
+    senha: {
+        type: String, 
+        required: true,
+        select: false
+    },
+    criadoEm:{
+        type: Date,
+        default: Date.now
+    },
+    
 })
 
 const usuarios = mongoose.model('usuarios', usuarioSchema)
